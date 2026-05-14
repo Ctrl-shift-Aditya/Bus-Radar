5f2cec8ae49aeb587d18300fffe7c37730dc70b9
Aditya Jaiswal <127968862+Ctrl-shift-Aditya@users.noreply.github.com>
2026-05-14 23:33:11 +0530

    Updated with Firebase
    Added Firebase SDK and set up the database to share location with many devices

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import {
+    getDatabase,
+    ref,
+    set,
+    onValue
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
+
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBi6J7XXCB-8wYP6U3VjDivJpbYZ0oeD2w",
+
+    authDomain: "bus-radar-mvp.firebaseapp.com",
+
+    databaseURL: "https://bus-radar-mvp-default-rtdb.firebaseio.com",
+
+    projectId: "bus-radar-mvp",
+
+    storageBucket: "bus-radar-mvp.firebasestorage.app",
+
+    messagingSenderId: "784601192060",
+
+    appId: "1:784601192060:web:549753dcb64972ef009bf9"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+
+
 const button = document.getElementById("locationBtn");
+
 const output = document.getElementById("output");
+
+
 
 button.addEventListener("click", () => {
 
@@ -8,16 +47,40 @@ button.addEventListener("click", () => {
         (position) => {
 
             const latitude = position.coords.latitude;
+
             const longitude = position.coords.longitude;
 
-            output.textContent =
-                `Latitude: ${latitude}, Longitude: ${longitude}`;
-        },
 
-        (error) => {
-            output.textContent = error.message;
+
+            set(ref(db, "location"), {
+
+                latitude: latitude,
+
+                longitude: longitude
+
+            });
+
         }
 
     );
+
+});
+
+
+
+const locationRef = ref(db, "location");
+
+
+
+onValue(locationRef, (snapshot) => {
+
+    const data = snapshot.val();
+
+
+
+    output.textContent =
+
+        `Latitude: ${data.latitude},
+         Longitude: ${data.longitude}`;
 
 });
